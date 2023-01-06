68f10fe748926e16b869f42b40760e9dbe8928bd
Lamine Mokhtar Jemli <113144795+laminemokhtarjemli@users.noreply.github.com>
2023-01-06 09:09:55 +0100

    Update 11-need_meeting.sql

--- 0x0C-MySQL_Advanced/11-need_meeting.sql
+++ 0x0C-MySQL_Advanced/11-need_meeting.sql
@@ -1,8 +1,4 @@
--- script that lists all students with a score under 80 and no meetings in the past month
-DROP VIEW IF EXISTS need_meeting;
 CREATE VIEW need_meeting AS
-SELECT name
-FROM students
-WHERE SCORE < 80
-AND last_meeting IS NULL
-OR last_meeting < dte_SUB(NOW(), INTERVAL 1 MONTH);
+  SELECT name
+  FROM students
+  WHERE score < 80 AND (last_meeting IS NULL OR last_meeting < DATEADD(month, -1, GETDATE()));
