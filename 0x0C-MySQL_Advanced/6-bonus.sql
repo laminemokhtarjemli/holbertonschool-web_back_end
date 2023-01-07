d0236e1f84174649f1034b441dc85e819753b54a
Lamine Mokhtar Jemli <113144795+laminemokhtarjemli@users.noreply.github.com>
2023-01-07 09:32:22 +0100

    Update 6-bonus.sql

--- 0x0C-MySQL_Advanced/6-bonus.sql
+++ 0x0C-MySQL_Advanced/6-bonus.sql
@@ -1,29 +1,19 @@
--- script that creates a procedure AddBonus
--- that adds a new correction.
-DELIMITER
-DROP PROCEDURE IF EXISTS AddBonus;
-CREATE PROCEDURE AddBonus(
-  IN user_id INT,
-  IN project_name VARCHAR(225),
-  IN score INT
-) BEGIN IF EXISTS(
-  SELECT *
-  FROM projects
-  WHERE name = project_name
-  LIMIT 1
-) THEN
-SET @pr_id = (
-    SELECT id
-    FROM projects
-    WHERE name = project_name
-    LIMIT 1
-  );
-ELSE
-INSERT INTO projects (name)
-VALUES (project_name);
-SET @pr_id = LAST_INSERT_ID();
-END IF;
-INSERT INTO corrections (user_id, pr_id, score)
-VALUES (user_id, @pr_id, score);
-END //
-DELIMITER;
+DELIMITER $$
+
+CREATE PROCEDURE AddBonus (IN user_id INT, IN project_name VARCHAR(255), IN score INT)
+BEGIN
+  -- Check if the project name already exists in the projects table
+  DECLARE project_id INT;
+  SELECT id INTO project_id FROM projects WHERE name = project_name;
+
+  -- If the project does not exist, insert a new row into the projects table
+  IF project_id IS NULL THEN
+    INSERT INTO projects (name) VALUES (project_name);
+    SET project_id = LAST_INSERT_ID();
+  END IF;
+
+  -- Insert a new row into the corrections table with the provided user_id, project_id, and score
+  INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, project_id, score);
+END$$
+
+DELIMITER ;
