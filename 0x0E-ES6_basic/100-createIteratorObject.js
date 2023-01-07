0a6383061e155deacbc0a32168dada6dbe799999
Lamine Mokhtar Jemli <113144795+laminemokhtarjemli@users.noreply.github.com>
2023-01-07 09:15:53 +0100

    Update 100-createIteratorObject.js

--- 0x0E-ES6_basic/100-createIteratorObject.js
+++ 0x0E-ES6_basic/100-createIteratorObject.js
@@ -1,25 +1,31 @@
 export default function createIteratorObject(report) {
-    const departments = Object.keys(report.allEmployees);
-    let currentDepartmentIndex = 0;
-    let currentEmployeeIndex = 0;
-    
-    return {
-    next() {
+let iterable = [];
+
+if (!report.allEmployees || typeof report.allEmployees !== 'object') {
+return iterable;
+}
+
+iterable = {
+*Symbol.iterator {
+const departments = Object.keys(report.allEmployees);
+let currentDepartmentIndex = 0;
+let currentEmployeeIndex = 0;
+      while (currentDepartmentIndex < departments.length) {
     const currentDepartment = departments[currentDepartmentIndex];
     const employees = report.allEmployees[currentDepartment];
     const employee = employees[currentEmployeeIndex];
+
     if (currentEmployeeIndex >= employees.length - 1) {
-        currentEmployeeIndex = 0;
-        currentDepartmentIndex++;
-      } else {
-        currentEmployeeIndex++;
-      }
-    
-      if (currentDepartmentIndex >= departments.length) {
-        return { done: true };
-      }
-    
-      return { value: employee, done: false };
-    },
+      currentEmployeeIndex = 0;
+      currentDepartmentIndex++;
+    } else {
+      currentEmployeeIndex++;
+    }
+
+    yield employee;
+  }
+},
 };
+
+return iterable;
 }
