8645c27a8caaa613aa339cac432f815c9ca280b3
Lamine Mokhtar Jemli <113144795+laminemokhtarjemli@users.noreply.github.com>
2023-01-07 21:09:20 +0100

    Update 3-all.js

--- 0x0F-ES6_promise/3-all.js
+++ 0x0F-ES6_promise/3-all.js
@@ -3,9 +3,9 @@ import { uploadPhoto, createUser } from "./utils.js";
 function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then(([uploadResponse, userResponse]) => {
-      console.log(uploadResponse.body, userResponse.firstName, userResponse.lastName);
+      console.log('${uploadResponse[0].body}, ${userResponse[1].firstName}, ${userResponse[1].lastName}');
     })
-    .catch(error => {
+    .catch(() => {
       console.log('Signup system offline');
     });
 }
