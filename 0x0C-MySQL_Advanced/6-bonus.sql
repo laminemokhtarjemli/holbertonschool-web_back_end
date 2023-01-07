bf2d42091d38c6d89996a9a0b98136ff528e0023
Lamine Mokhtar Jemli <113144795+laminemokhtarjemli@users.noreply.github.com>
2023-01-07 09:33:28 +0100

    Update 6-bonus.sql

--- 0x0C-MySQL_Advanced/6-bonus.sql
+++ 0x0C-MySQL_Advanced/6-bonus.sql
@@ -1,19 +1,18 @@
-DELIMITER $$
-
-CREATE PROCEDURE AddBonus (IN user_id INT, IN project_name VARCHAR(255), IN score INT)
+DELIMITER |
+CREATE PROCEDURE AddBonus (
+    IN user_id int,
+    IN project_name varchar(255),
+    IN score float
+)
 BEGIN
-  -- Check if the project name already exists in the projects table
-  DECLARE project_id INT;
-  SELECT id INTO project_id FROM projects WHERE name = project_name;
+    INSERT INTO projects (name)
+    SELECT project_name FROM DUAL
 
-  -- If the project does not exist, insert a new row into the projects table
-  IF project_id IS NULL THEN
-    INSERT INTO projects (name) VALUES (project_name);
-    SET project_id = LAST_INSERT_ID();
-  END IF;
-
-  -- Insert a new row into the corrections table with the provided user_id, project_id, and score
-  INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, project_id, score);
-END$$
-
-DELIMITER ;
+    WHERE NOT EXISTS 
+    (SELECT * FROM projects WHERE name = project_name);
+    INSERT INTO corrections (user_id, project_id, score)
+    VALUES (user_id, 
+            (SELECT id FROM projects WHERE name = project_name), 
+            score);
+END;
+|
