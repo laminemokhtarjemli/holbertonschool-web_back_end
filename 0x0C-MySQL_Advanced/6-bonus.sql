bb164793696f96979b6d2dc04b069a061a14fc47
Lamine Mokhtar Jemli <113144795+laminemokhtarjemli@users.noreply.github.com>
2023-01-07 09:34:58 +0100

    Update 6-bonus.sql

--- 0x0C-MySQL_Advanced/6-bonus.sql
+++ 0x0C-MySQL_Advanced/6-bonus.sql
@@ -1,18 +1,19 @@
-DELIMITER |
-CREATE PROCEDURE AddBonus (
-    IN user_id int,
-    IN project_name varchar(255),
-    IN score float
+DELIMITER //
+CREATE PROCEDURE AddBonus(
+    IN `user_id` INT,
+    IN `project_name` VARCHAR(255),
+    IN `score` INT
 )
 BEGIN
-    INSERT INTO projects (name)
-    SELECT project_name FROM DUAL
+    INSERT INTO `projects`(`name`)
+    SELECT `project_name`
+    FROM DUAL
+    WHERE `project_name` NOT IN (
+        SELECT `name` FROM `projects`
+    );
 
-    WHERE NOT EXISTS 
-    (SELECT * FROM projects WHERE name = project_name);
-    INSERT INTO corrections (user_id, project_id, score)
-    VALUES (user_id, 
-            (SELECT id FROM projects WHERE name = project_name), 
-            score);
-END;
-|
+    INSERT INTO `corrections`(`user_id`, `project_id`, `score`)
+    VALUES (
+        `user_id`, (SELECT `id` FROM `projects` WHERE `name` = `project_name`), `score`
+    );
+END; //
