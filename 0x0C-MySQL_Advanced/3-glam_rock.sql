88a1fe812f14e205d6b36e35a0787945c632a2f5
Lamine Mokhtar Jemli <113144795+laminemokhtarjemli@users.noreply.github.com>
2023-01-07 09:25:31 +0100

    Update 3-glam_rock.sql

--- 0x0C-MySQL_Advanced/3-glam_rock.sql
+++ 0x0C-MySQL_Advanced/3-glam_rock.sql
@@ -1,8 +1,4 @@
-CREATE VIEW band_lifespans AS
-  SELECT band_name, (split - formed) AS lifespan
-  FROM metal_bands;
-
-    SELECT band_name, lifespan
-FROM band_lifespans
+SELECT band_name, (split - formed) AS lifespan
+FROM metal_bands
 WHERE main_style = 'Glam rock'
 ORDER BY lifespan DESC;
