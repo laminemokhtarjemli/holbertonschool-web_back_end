fa579e42dd86d42692600ed90f7532e5210ee843
Lamine Mokhtar Jemli <113144795+laminemokhtarjemli@users.noreply.github.com>
2023-01-07 22:19:15 +0100

    Update 12-createReportObject.js

--- 0x0E-ES6_basic/12-createReportObject.js
+++ 0x0E-ES6_basic/12-createReportObject.js
@@ -2,7 +2,7 @@ export default function createReportObject(employeesList) {
     return {
     allEmployees: { ...employeesList },
     getNumberOfDepartments(employeesList) {
-    return Object.keys(this.employeesList).length;
+    return Object.keys(employeesList).length;
     },
     };
     }
