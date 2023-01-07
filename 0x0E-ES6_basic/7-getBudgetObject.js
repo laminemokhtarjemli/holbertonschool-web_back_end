265b9a9fc8c8f9957763c567127289b2fdc51ddc
Lamine Mokhtar Jemli <113144795+laminemokhtarjemli@users.noreply.github.com>
2023-01-07 08:45:57 +0100

    Update 7-getBudgetObject.js

--- 0x0E-ES6_basic/7-getBudgetObject.js
+++ 0x0E-ES6_basic/7-getBudgetObject.js
@@ -1,3 +1,9 @@
-export default function getBudgetObject({income = '$0', gdp = '$0', capita = '$0'} = {}) {
-return {income, gdp, capita};
+export default function getBudgetObject(income, gdp, capita) {
+const budget = {
+income,
+gdp,
+capita,
+};
+
+return budget;
 }
