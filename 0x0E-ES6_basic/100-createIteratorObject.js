484d1d2d64b530f85a3d5f0232023bf5ca0b0fa1
Lamine Mokhtar Jemli <113144795+laminemokhtarjemli@users.noreply.github.com>
2023-01-07 22:25:23 +0100

    Update 100-createIteratorObject.js

--- 0x0E-ES6_basic/100-createIteratorObject.js
+++ 0x0E-ES6_basic/100-createIteratorObject.js
@@ -1,8 +1,3 @@
-export default function createIteratorObject(report) {
-  const result = [];
-  const values = Object.values(report.allEmployees);
-  for (let i = 0; i < values.length; i++) {
-    result.push(...values[i]);
-  }
-  return result;
+export default function iterateThroughObject(reportWithIterator) {
+  return reportWithIterator.toString();
 }
