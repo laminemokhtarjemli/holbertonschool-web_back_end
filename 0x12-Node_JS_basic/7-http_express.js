1fadb7c8ca1c2c4db3a5d85e1169322461dd9fac
Lamine Mokhtar Jemli <113144795+laminemokhtarjemli@users.noreply.github.com>
2023-01-13 17:05:21 +0100

    Update 7-http_express.js

--- 0x12-Node_JS_basic/7-http_express.js
+++ 0x12-Node_JS_basic/7-http_express.js
@@ -5,22 +5,23 @@ const app = express();
 const port = 1245;
 
 app.get('/', (req, res) => {
-  res.send('Hello Holberton School!');
+res.send('Hello Holberton School!');
 });
 
 app.get('/students', (req, res) => {
-  const database = process.argv[2];
-  fs.readFile(database, 'utf8', (err, data) => {
-    if (err) throw err;
-    let students = data.split('\n');
-    students = students.filter(student => student !== '');
-    res.send(`This is the list of our students: ${students.join(', ')}`);
-  });
+fs.readFile(process.argv[2], 'utf8', (err, data) => {
+if (err) {
+res.send('Error: Cannot load the database');
+return;
+}
+let students = data.split('\n');
+students = students.filter(student => student !== '');
+res.send(This is the list of our students: ${students.join(', ')});
+});
 });
 
 app.listen(port, () => {
-  console.log(`HTTP server listening on port ${port}`);
+console.log(HTTP server listening on port ${port});
 });
 
 module.exports = app;
-
