49937337592227c0a044a1934d9075ec9f425a54
Lamine Mokhtar Jemli <113144795+laminemokhtarjemli@users.noreply.github.com>
2023-01-14 20:44:39 +0100

    Update 0-calcul.js

--- 0x13-unittests_in_js/0-calcul.js
+++ 0x13-unittests_in_js/0-calcul.js
@@ -1,4 +1,7 @@
-const  calculateNumber = (a, b) =>{
-    return Math.round(a) + Math.round(b);
-};
+const calculateNumber = (a, b) => {
+if (!Number.isFinite(a) || !Number.isFinite(b)) {
+throw new TypeError();
+}
+return Math.round(a) + Math.round(b);
+}
 module.exports = calculateNumber;
