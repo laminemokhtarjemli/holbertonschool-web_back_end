90d1ded8031959b2d27f544d30716c6348f462a3
Lamine Mokhtar Jemli <113144795+laminemokhtarjemli@users.noreply.github.com>
2023-01-14 20:34:20 +0100

    Add files via upload

---
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,18 @@
+const assert = require('assert');
+
+describe('calculateNumber', () => {
+    it('should round the input numbers and return their sum', () => {
+        assert.strictEqual(calculateNumber(3.4, 5.6), 9);
+        assert.strictEqual(calculateNumber(6.5, 8.5), 15);
+    });
+
+    it('should work correctly for negative numbers', () => {
+        assert.strictEqual(calculateNumber(-3.4, -5.6), -9);
+        assert.strictEqual(calculateNumber(-6.5, -8.5), -15);
+    });
+
+    it('should work correctly for numbers with decimal places', () => {
+        assert.strictEqual(calculateNumber(3.14, 2.72), 6);
+        assert.strictEqual(calculateNumber(2.99, 3.01), 6);
+    });
+});
