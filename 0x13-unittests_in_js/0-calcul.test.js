2f708112d15286041e25a67ff482e71bf9952d16
Lamine Mokhtar Jemli <113144795+laminemokhtarjemli@users.noreply.github.com>
2023-01-23 10:50:28 +0100

    Update 0-calcul.test.js

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -1,7 +1,6 @@
 const { calculateNumber } = require('./0-calcul');
 const assert = require('assert');
 
-
 describe('calculateNumber', () => {
     it('should return the rounded sum of a and b', () => {
         assert.equal(calculateNumber(2.3, 4.6), 7);
@@ -18,4 +17,9 @@ describe('calculateNumber', () => {
         assert.equal(calculateNumber(0, -2.9), -3);
         assert.equal(calculateNumber(0, 5.4), 5);
     });
+    it('returns TypeError', () => {
+        assert.throws(() => calculateNumber(NaN, 5.6), { name: 'TypeError' });
+        assert.throws(() => calculateNumber(5.6, NaN), { name: 'TypeError' });
+        assert.throws(() => calculateNumber(NaN, NaN), { name: 'TypeError' });
+      });
 });
