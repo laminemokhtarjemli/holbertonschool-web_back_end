61ad83718323d68e628cca0cee7cd7ae04df435c
Lamine Mokhtar Jemli <113144795+laminemokhtarjemli@users.noreply.github.com>
2023-01-23 10:52:21 +0100

    Update 0-calcul.test.js

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -3,19 +3,19 @@ const assert = require('assert');
 
 describe('calculateNumber', () => {
     it('should return the rounded sum of a and b', () => {
-        assert.equal(calculateNumber(2.3, 4.6), 7);
-        assert.equal(calculateNumber(3.5, 2.9), 6);
-        assert.equal(calculateNumber(1.6, 5.4), 7);
+        assert.strictEqual(calculateNumber(2.3, 4.6), 7);
+        assert.strictEqual(calculateNumber(3.5, 2.9), 6);
+        assert.strictEqual(calculateNumber(1.6, 5.4), 7);
     });
     it('should return the rounded sum of a and b even if a is negative', () => {
-        assert.equal(calculateNumber(-2.3, 4.6), 2);
-        assert.equal(calculateNumber(-3.5, 2.9), 0);
-        assert.equal(calculateNumber(-1.6, 5.4), 4);
+        assert.strictEqual(calculateNumber(-2.3, 4.6), 2);
+        assert.strictEqual(calculateNumber(-3.5, 2.9), 0);
+        assert.strictEqual(calculateNumber(-1.6, 5.4), 4);
     });
     it('should return the rounded sum of a and b even if a is 0', () => {
-        assert.equal(calculateNumber(0, 4.6), 5);
-        assert.equal(calculateNumber(0, -2.9), -3);
-        assert.equal(calculateNumber(0, 5.4), 5);
+        assert.strictEqual(calculateNumber(0, 4.6), 5);
+        assert.strictEqual(calculateNumber(0, -2.9), -3);
+        assert.strictEqual(calculateNumber(0, 5.4), 5);
     });
     it('returns TypeError', () => {
         assert.throws(() => calculateNumber(NaN, 5.6), { name: 'TypeError' });
