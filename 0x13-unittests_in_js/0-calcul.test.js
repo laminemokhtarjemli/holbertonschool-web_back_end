8dc3a656646dccc39f8456cf98dffb22b5b38807
Lamine Mokhtar Jemli <113144795+laminemokhtarjemli@users.noreply.github.com>
2023-02-15 20:28:26 +0100

    Update 0-calcul.test.js

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -1,23 +1,23 @@
-const calculateNumber = require("./0-calcul.js");
-const assert = require("assert");
+const assert = require('assert');
+const calculateNumber = require('./0-calcul.js');
 
 describe('calculateNumber', () => {
-  it('returns rounded positive sum', () => {
-    assert.strictEqual(calculateNumber(4, 8), 12);
-    assert.strictEqual(calculateNumber(1.9, 0), 2);
-    assert.strictEqual(calculateNumber(6.1, 6.1), 12);
-    assert.strictEqual(calculateNumber(0.1, 0.2), 0);
-    assert.strictEqual(calculateNumber(0.1, 0.6), 1);
+  it('checks the output', () => {
+    assert.equal(calculateNumber(1.5, 3.7), 6);
+    assert.strictEqual(calculateNumber(1, 3), 4);
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+    assert.strictEqual(calculateNumber(3.7, 1), 5);
+    assert.strictEqual(calculateNumber(3.7, 1.2), 5);
   });
-
-  it('returns rounded negative sum', () => {
-    assert.strictEqual(calculateNumber(-1, -3), -4);
-    assert.strictEqual(calculateNumber(-1.4, -3.6), -5);
+  it('negative numbers', () => {
+    assert.strictEqual(calculateNumber(-1, 1), 0);
+    assert.strictEqual(calculateNumber(1, -1), 0);
+    assert.strictEqual(calculateNumber(-1, -1), -2);
   });
-
-  it('returns TypeError', () => {
-    assert.throws(() => calculateNumber(NaN, 5.6), { name: 'TypeError' });
-    assert.throws(() => calculateNumber(5.6, NaN), { name: 'TypeError' });
-    assert.throws(() => calculateNumber(NaN, NaN), { name: 'TypeError' });
+  it('checks arguments', () => {
+    assert.strictEqual(isNaN(calculateNumber(1)), true);
+    assert.strictEqual(isNaN(calculateNumber()), true);
   });
 });
