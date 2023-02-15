574f1157d4aa61f7a5fcf0b0844843872424758e
Lamine Mokhtar Jemli <113144795+laminemokhtarjemli@users.noreply.github.com>
2023-02-15 20:27:23 +0100

    Update 0-calcul.test.js

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -1,25 +1,23 @@
-const { calculateNumber } = require('./0-calcul');
-const assert = require('assert');
+const calculateNumber = require("./0-calcul.js");
+const assert = require("assert");
 
 describe('calculateNumber', () => {
-    it('should return the rounded sum of a and b', () => {
-        assert.strictEqual(calculateNumber(2.3, 4.6), 7);
-        assert.strictEqual(calculateNumber(3.5, 2.9), 6);
-        assert.strictEqual(calculateNumber(1.6, 5.4), 7);
-    });
-    it('should return the rounded sum of a and b even if a is negative', () => {
-        assert.strictEqual(calculateNumber(-2.3, 4.6), 2);
-        assert.strictEqual(calculateNumber(-3.5, 2.9), 0);
-        assert.strictEqual(calculateNumber(-1.6, 5.4), 4);
-    });
-    it('should return the rounded sum of a and b even if a is 0', () => {
-        assert.strictEqual(calculateNumber(0, 4.6), 5);
-        assert.strictEqual(calculateNumber(0, -2.9), -3);
-        assert.strictEqual(calculateNumber(0, 5.4), 5);
-    });
-    it('returns TypeError', () => {
-        assert.throws(() => calculateNumber(NaN, 5.6), { name: 'TypeError' });
-        assert.throws(() => calculateNumber(5.6, NaN), { name: 'TypeError' });
-        assert.throws(() => calculateNumber(NaN, NaN), { name: 'TypeError' });
-      });
+  it('returns rounded positive sum', () => {
+    assert.strictEqual(calculateNumber(4, 8), 12);
+    assert.strictEqual(calculateNumber(1.9, 0), 2);
+    assert.strictEqual(calculateNumber(6.1, 6.1), 12);
+    assert.strictEqual(calculateNumber(0.1, 0.2), 0);
+    assert.strictEqual(calculateNumber(0.1, 0.6), 1);
+  });
+
+  it('returns rounded negative sum', () => {
+    assert.strictEqual(calculateNumber(-1, -3), -4);
+    assert.strictEqual(calculateNumber(-1.4, -3.6), -5);
+  });
+
+  it('returns TypeError', () => {
+    assert.throws(() => calculateNumber(NaN, 5.6), { name: 'TypeError' });
+    assert.throws(() => calculateNumber(5.6, NaN), { name: 'TypeError' });
+    assert.throws(() => calculateNumber(NaN, NaN), { name: 'TypeError' });
+  });
 });
