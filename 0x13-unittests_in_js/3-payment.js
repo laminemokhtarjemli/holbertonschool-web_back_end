c33573d3b1fdc9bec239919c81d8f2631c9cd920
Lamine Mokhtar Jemli <113144795+laminemokhtarjemli@users.noreply.github.com>
2023-02-16 19:46:30 +0100

    Add files via upload

---
+++ 0x13-unittests_in_js/3-payment.js
@@ -0,0 +1,10 @@
+const { util } = require("chai");
+const Utils = require('./utils');
+
+function sendPaymentRequestToApi(totalAmount, totalShipping) {
+  const sum = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+  console.log(`The total is: ${sum}`);
+  return sum;
+};
+module.exports = sendPaymentRequestToApi
+
